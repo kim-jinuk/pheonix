a6df2693ed68e7bce393846ce1d1da6a4811dfe5
mooooonoe <msb3536@naver.com>
2025-06-19 15:20:14 +0900

    tcp boost libaray update

--- HD_zynq/include/running_control_csc/receiver.hpp
+++ HD_zynq/include/running_control_csc/receiver.hpp
@@ -1,46 +1,38 @@
+#ifndef RECEIVER_BOOST_HPP
+#define RECEIVER_BOOST_HPP
 
-#ifndef TCPRECEIVER_HPP
-#define TCPRECEIVER_HPP
+#include <boost/asio.hpp>
+#include <iostream>
+#include <cstdint>
 
-
-
-#include <netinet/in.h>
-#include <string>
 #include <running_control_csc/globals.hpp>
 
 
-
 class TcpBase {
-
-protected :
-    int server_sock; // listen sock
-    int client_sock;
-    sockaddr_in server_addr;
-    sockaddr_in client_addr;
-    socklen_t client_len;
+protected:
+    boost::asio::io_context io_context_;
+    boost::asio::ip::tcp::acceptor acceptor_;
+    std::unique_ptr<boost::asio::ip::tcp::socket> socket_;
 
 public:
     TcpBase(int port);
-    ~TcpBase();
+    virtual ~TcpBase();
+
     bool AcceptConnection();
 };
 
-
 class TcpCmdChannel : public TcpBase {
-public :
+public:
     TcpCmdChannel(int port);
-
     bool TcpParsing(TcpCommand& cmd);
-    bool sendAck(TcpCommand& cmd);
+    bool sendAck(const TcpCommand& cmd);
     void disconnect_sock();
-
 };
 
 class TcpStateChannel : public TcpBase {
-public :
+public:
     TcpStateChannel(int port);
-
-    bool sendState(TcpState& stateinfo);
+    bool sendState(const TcpState& state);
 };
 
 #endif
